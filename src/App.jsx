ab4400b62011658e2015aa84d78525d00eaa4e37
nkhaccuong <cuong19012002@gmail.com>
2025-09-22 01:04:57 +0700

    feat: add gsap animate scroll trigger in hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import HeroSection from './sections/HeroSection'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
   return (
     <main>
       <NavBar/>
       <HeroSection/>
+      <div className="dvh bg-red-500 h-dvh"></div>
     </main>
   )
 }
